7d26c8a9964803af376af8e40853938e615e72f4
Kirk Hauck <kirkihauck@gmail.com>
2023-04-19 12:38:03 -0600

    style: Make header sticky and center pokeball logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,10 @@ import pokeballLogo from '../../assets/pokeball.png'
 const Header = () => {
   return (
     <header>
+      <div className='logo-container'>
         <h1 className='main-logo'>PixelDex</h1>
         <img className='pokeball-logo' src={pokeballLogo} alt='pixelated pokeball logo' />
+      </div>
     </header>
   );
 };
