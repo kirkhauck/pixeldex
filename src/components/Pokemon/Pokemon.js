05052bdd02e8b8cb9fc6db588ebe04009c93ad35
Kirk Hauck <kirkihauck@gmail.com>
2023-04-19 10:17:45 -0600

    feat: Renamed SearchView to SpriteView and added Pokemon and PokemonContainer directories and files

---
+++ src/components/Pokemon/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Pokemon.css';
+
+const Pokemon = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Pokemon;
---
+++ src/components/PokemonContainer/PokemonContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PokemonContainer.css';
+
+const PokemonContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default PokemonContainer;
