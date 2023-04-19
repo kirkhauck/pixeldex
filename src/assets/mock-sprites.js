7976f18547fd5a3506e84529fb782a979a1e5452
Kirk Hauck <kirkihauck@gmail.com>
2023-04-18 19:03:37 -0600

    asset: Add mock sprite data for wireframing and styling

---
+++ src/assets/mock-sprites.js
@@ -0,0 +1,55 @@
+export const frontSprites = [
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png',
+];
+
+export const bulbasaur = {
+back_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+back_female: null,
+back_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png',
+back_shiny_female: null,
+front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+front_female: null,
+front_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png',
+front_shiny_female: null
+}
+
+export const charmander = {
+back_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png',
+back_female: null,
+back_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/4.png',
+back_shiny_female: null,
+front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+front_female: null,
+front_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/4.png',
+front_shiny_female: null
+}
+
+export const squirtle = {
+back_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png',
+back_female: null,
+back_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/7.png',
+back_shiny_female: null,
+front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+front_female: null,
+front_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/7.png',
+front_shiny_female: null
+}
