be504ee6990b1d459c388768f1e4b6fc5129b552
Kirk Hauck <kirkihauck@gmail.com>
2023-04-19 16:50:33 -0600

    feat: Setup routes in App with mock path for SpriteView

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from '../Header/Header';
 import HomeView from '../HomeView/HomeView';
 import SpriteView from '../SpriteView/SpriteView';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <Header />
-      {/* <HomeView /> */}
-      <SpriteView />
+      <Header/>
+      <Routes>
+        <Route path='/' element={<HomeView/>} />
+        {/* replace bublasaur with dynamic route once API data is used to render pokemon */}
+        <Route path='/bulbasaur' element={<SpriteView/>} />
+      </Routes>
     </>
   );
 }
