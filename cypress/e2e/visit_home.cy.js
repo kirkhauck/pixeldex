f8a93b6d93038b3bd0e8e8ff8901cb3c04e7c99a
Kirk Hauck <kirkihauck@gmail.com>
2023-04-20 16:56:51 -0600

    test: Test Header to have all correct content

---
+++ cypress/e2e/visit_home.cy.js
@@ -0,0 +1,23 @@
+describe('Visit Home Page', () => {
+  beforeEach('ntercept and visit homepage', () => {
+    cy.loadPage();
+  })
+
+  it('should have a header with a title, subtitle, pokeball logo, and Home button', () => {
+    cy.get('header')
+      .get('.title-logo')
+      .should('have.attr', 'src', '/static/media/title-logo.4eb791ae459a4d4e9f0f.png')
+      .should('have.attr', 'alt', 'pixeldex title logo')
+      .should('be.visible')
+      
+    cy.get('header').contains('p', 'Unleash the pixels, catch the nostalgia!')
+    
+    cy.get('header')
+      .get('.pokeball-logo')
+      .should('have.attr', 'src', '/static/media/pokeball-logo.178c73ab034c28222c35.png')
+      .should('have.attr', 'alt', 'pixelated pokeball logo')
+      
+    cy.get('header')
+    .contains('button', 'HOME');
+  });
+});
