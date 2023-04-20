26844d4a984dc6c239609ac5de44d97e6054a708
Kirk Hauck <kirkihauck@gmail.com>
2023-04-20 12:12:38 -0600

    feat: SearchBar text input is now state controlled

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 const SearchBar = () => {
+  const [pokemonName, setPokemonName] = useState('')
+
   return (
     <form>
-      <input type='text' />
+      <input
+        type='text'
+        name='pokemonName'
+        value={pokemonName}
+        onChange={event => setPokemonName(event.target.value)}
+      />
       <button className='search-button'>SEARCH</button>
     </form>
   );
