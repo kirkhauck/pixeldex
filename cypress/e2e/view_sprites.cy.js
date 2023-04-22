010de211378ce23d3923dc626233b7dd393259c6
Kirk Hauck <kirkihauck@gmail.com>
2023-04-22 12:34:36 -0600

    test: Test visit to Pokemon sprite page and check URL path and header

--- cypress/e2e/view_sprites.cy.js
+++ cypress/e2e/view_sprites.cy.js
@@ -3,7 +3,28 @@ describe('Visit Sprite Page', () => {
     cy.visitHome()
   });
 
-  it('', () => {
+  it('Should visit a Pokemon\'s sprite page when it is clicked', () => {
+    cy.get('.pokemon-container > div > :nth-child(1)').click()
+      .url().should('eq', 'http://localhost:3000/bulbasaur')
+  });
 
+  it('Should have a header with a title, subtitle, pokeball logo, and Home button', () => {
+    cy.get('header')
+      .find('.title-logo')
+      .should('have.attr', 'src', '/static/media/title-logo.4eb791ae459a4d4e9f0f.png')
+      .should('have.attr', 'alt', 'pixeldex title logo')
+      .should('be.visible');
+      
+      cy.get('header')
+      .contains('p', 'Unleash the pixels, catch the nostalgia!');
+      
+      cy.get('header')
+      .find('.pokeball-logo')
+      .should('have.attr', 'src', '/static/media/pokeball-logo.178c73ab034c28222c35.png')
+      .should('have.attr', 'alt', 'pixelated pokeball logo')
+      .should('be.visible');
+      
+    cy.get('header')
+    .contains('button', 'HOME');
   });
 });
