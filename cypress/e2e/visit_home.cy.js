735b73923caa13de84df3628b7b4bd7d1960a30a
Kirk Hauck <kirkihauck@gmail.com>
2023-04-23 14:32:38 -0600

    refac: Fix visit_home indentation and punctuation

--- cypress/e2e/visit_home.cy.js
+++ cypress/e2e/visit_home.cy.js
@@ -1,7 +1,7 @@
 describe('Visit Home Page', () => {
   beforeEach('intercept and stub homepage', () => {
     cy.visitHome();
-  })
+  });
 
   it('should have a header with a title, subtitle, pokeball logo, and Home button', () => {
     cy.get('header')
@@ -10,17 +10,17 @@ describe('Visit Home Page', () => {
       .should('have.attr', 'alt', 'pixeldex title logo')
       .should('be.visible');
       
-      cy.get('header')
+    cy.get('header')
       .contains('p', 'Catch \'em all! One pixel at a time.');
       
-      cy.get('header')
+    cy.get('header')
       .find('.pokeball-logo')
       .should('have.attr', 'src', '/static/media/pokeball-logo.178c73ab034c28222c35.png')
       .should('have.attr', 'alt', 'pixelated pokeball logo')
       .should('be.visible');
       
     cy.get('header')
-    .contains('button', 'HOME');
+      .contains('button', 'HOME');
   });
 
   it('should have a form with an empty text input and search button', () => {
@@ -50,16 +50,16 @@ describe('Visit Home Page', () => {
       .should('have.attr', 'src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png')
       .should('be.visible')
       .siblings('figcaption')
-      .contains('p', '#0001 | BULBASAUR')
+      .contains('p', '#0001 | BULBASAUR');
       
-      cy.get('.pokemon-container > div').children().last()
+    cy.get('.pokemon-container > div').children().last()
       .find('a')
       .should('have.attr', 'href', '/raticate')
       .find('img')
       .should('have.attr', 'src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png')
       .should('be.visible')
       .siblings('figcaption')
-      .contains('p', '#0020 | RATICATE')
+      .contains('p', '#0020 | RATICATE');
   });
 });
 
